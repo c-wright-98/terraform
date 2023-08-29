8ab50b698f0d8ab2e3aefd7fedb24c34188fc3bc
Ubuntu <ubuntu@ip-172-31-37-159.eu-west-2.compute.internal>
2023-08-29 14:48:49 +0000

    Added resource

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ provider "aws" {
     secret_key = "3gSerUT5rreC989K5l4f3WcGZ0yUNaltaw4C8r/1"
     region = "eu-west-2"
 }
+
+resource "aws_instance" "example" {
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
+}
